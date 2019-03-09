dae89f5e01eacd4f5e78e5b148b855bb28527c06
Guilherme <ribeiro@textkernel.nl>
2019-03-09 18:10:25 +0100

    Updates

--- src/components/CameraButton/CameraButton.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import './CameraButton.css';
-
-// tslint:disable-next-line:variable-name
-const CameraButton = () =>
-    <div className="camera-button"> Test </div>;
-
-export default CameraButton;
--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-// tslint:disable-next-line:import-name
-import * as React from 'react';
-import { render } from 'react-dom';
-import CameraButton from './components/CameraButton';
-
-render(
-    <CameraButton />,
-    document.getElementById('root'),
-);
