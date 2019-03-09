419fcbbc385a5c03e26b2928ccaa3c921cf572ad
Guilherme <ribeiro@textkernel.nl>
2019-03-09 20:34:54 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // Set this to true for production
-var doCache = false;
-var CACHE_NAME = "emojis-on-me-sw-cache-3";
+var doCache = true;
+var CACHE_NAME = "emojis-on-me-sw-cache-4";
 
 // Delete old caches that are not our current one!
 self.addEventListener("activate", event => {
