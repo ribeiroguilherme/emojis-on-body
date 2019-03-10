5938b990ea25b1659f6668babf645c014c9c0bf1
Guilherme Marquesini <guilhermemrr@gmail.com>
2019-03-10 22:32:46 +0100

    Streaming video to canvas

--- src/components/Camera/Camera.tsx
+++ src/components/Camera/Camera.tsx
@@ -13,6 +13,10 @@ class Camera extends React.PureComponent<Props, State> {
 
     videoElement = React.createRef<HTMLVideoElement>();
 
+    private canvasElement = React.createRef<HTMLCanvasElement>();
+
+    private canvasContext: CanvasRenderingContext2D = null;
+
     private currentStream: MediaStream = null;
 
     state: Readonly<State> = {
@@ -28,6 +32,39 @@ class Camera extends React.PureComponent<Props, State> {
         this.startCamera();
     }
 
+    timerCallback = () => {
+        if (this.videoElement.current.paused || this.videoElement.current.ended) {
+            return;
+        }
+
+        this.computeFrame();
+
+        setTimeout(() => this.timerCallback(), 0);
+    }
+
+    syncVideoWithCanvas() {
+        this.canvasContext = this.canvasElement.current.getContext('2d');
+        this.videoElement.current.addEventListener('play', this.timerCallback, false);
+    }
+
+    computeFrame() {
+        const { width, height } = this.props;
+        this.canvasContext.drawImage(this.videoElement.current, 0, 0, width, height);
+
+        const frame = this.canvasContext.getImageData(0, 0, width, height);
+        const l = frame.data.length / 4;
+
+        for (let i = 0; i < l; i += 1) {
+            const r = frame.data[i * 4 + 0];
+            const g = frame.data[i * 4 + 1];
+            const b = frame.data[i * 4 + 2];
+            if (g > 100 && r > 100 && b < 43) {
+                frame.data[i * 4 + 3] = 0;
+            }
+        }
+        this.canvasContext.putImageData(frame, 0, 0);
+    }
+
     async startCamera() {
 
         const stream = await this.requestMedia();
@@ -35,6 +72,8 @@ class Camera extends React.PureComponent<Props, State> {
         if (!stream) return;
 
         this.currentStream = stream;
+
+        this.syncVideoWithCanvas();
 
         this.videoElement.current.srcObject = stream;
         this.videoElement.current.play().then(() => {
@@ -75,13 +114,22 @@ class Camera extends React.PureComponent<Props, State> {
         }
 
         return (
-            <video
-                ref={this.videoElement}
-                height={height}
-                width={width}
-                playsInline
-                autoPlay
-            />
+            <React.Fragment>
+                <video
+                    ref={this.videoElement}
+                    height={height}
+                    width={width}
+                    playsInline
+                    autoPlay
+                    className="camera__video"
+                />
+                <canvas
+                    ref={this.canvasElement}
+                    height={height}
+                    width={width}
+                />
+
+            </React.Fragment>
         );
     }
 
